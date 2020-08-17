c15a30593f14b1e7f67db76131847dc58ab874de
Fatih Can <ybsfatihcan@gmail.com>
2020-08-17 13:38:08 +0300

    A sample API is complete with Node.js

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router
+
+const users = [{ name: 'Fatih', email: 'fatih@mail.com' }]
+
+router.get('/', (_, res) => {
+    res.send('Your Express App');
+});
+
+
+router.get('/user/:name', (req, res) => {
+    const { name } = req.params;
+    const user = users.filter((user) => user.name === name)[0];
+    res.json({ ok: true, user });
+});
+
+router.post('/adduser', (req, res) => {
+    const { name, email } = req.body;
+    if (name && email) {
+        users.push({ name, email });
+        res.json({ ok: true, users });
+
+    }
+});
+
+module.exports = router;
